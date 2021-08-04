cfe102ad5384569ce38ecf2700d6ff6e7c543b7c
Michele Ferracin <michele@ilclubdellesei.blog>
2021-08-04 12:21:47 +0200

    Inizio

---
+++ Ep-03-If/src/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace imparo_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci un numero intero");
+            string input = Console.ReadLine();
+
+            int numeroInserito = Convert.ToInt32(input);
+
+            if (numeroInserito % 2 == 0)
+            {
+                Console.WriteLine("Il numero inserito è pari.");
+            }
+            else
+            {
+                Console.WriteLine("Il numero inserito è dispari.");
+            }
+ 
+            Console.WriteLine("Premere INVIO per terminare.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Imparare a programmare
+
+Reposoitory di tutti gli esempi di codice presenti nel mio corso [Impara a programmare](https://www.youtube.com/watch?v=EldxRs4tnFI&list=PLU2FPKLp7ojLUg7rkYpm0Z1ykpmENI0-H) su YouTube 📽️.
+
+## Organizzazione
+
+A ogni episodio, dove necessario, corrisponde una cartella con il codice usato nel video.
+
+## Elenco video
+
+1. [Episodio 00](https://www.youtube.com/watch?v=EldxRs4tnFI&list=PLU2FPKLp7ojLUg7rkYpm0Z1ykpmENI0-H) - Il tuo primo programma da zero
+1. [Episodio 01](https://www.youtube.com/watch?v=qhgoBppt6l8&list=PLU2FPKLp7ojLUg7rkYpm0Z1ykpmENI0-H&index=2) - Istruzioni, metodi, variabili
+1. [Episodio 03]() - If
